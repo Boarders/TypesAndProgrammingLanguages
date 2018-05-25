dad80726d99f0f0215088bd312300c0c3535b927
Callan McGill <callan.mcgill@gmail.com>
2018-05-25 17:30:13 +0100

    Add back small step eval

--- chap3/ArithBool.hs
+++ chap3/ArithBool.hs
@@ -28,7 +28,27 @@ isVal = (||) <$> boolVal <*> numVal
 
 smallStepEval :: Term -> Maybe Term
 smallStepEval t = case t of
-  _ -> undefined
+  T -> Nothing
+  F -> Nothing
+  If t1 t2 t3 -> case t2 of
+                   T -> Just t2
+                   F -> Just t3
+                   _ -> If <$> (smallStepEval t1) <*> pure t2 <*> pure t3
+  Zero    -> Nothing
+  Succ t1 -> Succ <$> (smallStepEval t1)
+  Pred t  -> case t of
+               Zero -> Just Zero
+               Succ n | numVal n -> Just n
+               _ -> Pred <$> (smallStepEval t)
+  IsZero t -> case t of
+                Zero -> Just T
+                Succ n | numVal n -> Just F
+                _ -> IsZero <$> (smallStepEval t)
+
+eval :: Term -> Term
+eval t = case smallStepEval t of
+  Nothing -> t
+  Just t' -> eval t'
 
 bigStepEval :: Term -> Term
 bigStepEval term = case term of
